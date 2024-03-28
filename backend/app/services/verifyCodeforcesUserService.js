7ab539ad8fd1a5c9f2292ee2059949eeee0f14d9
sudesh003 <snsudeshnayak3@gmail.com>
2024-03-28 21:33:31 +0530

    codeforces user verified

--- backend/app/services/verifyCodeforcesUserService.js
+++ backend/app/services/verifyCodeforcesUserService.js
@@ -1,13 +1,45 @@
-const verify = async (id,index,userId) => {
+const axios = require("axios");
+
+async function myCallback(id, index, userId) {
     try {
+        const response = await axios.get('https://codeforces.com/api/user.status', {
+            params: { handle: userId, from: 1, count: 1 },
+        });
+    
+        const submittedId = response.data.result[0]['id'];
+        const submittedIndex = response.data.result[0]['problem']['index'];
         
-        return true;
+        if (submittedId == id && submittedIndex == index) {
+            return true;
+        } else {
+            return false;
+        }
     } catch (error) {
-      throw new Error('Failed to get hello message');
+        console.error('Failed to fetch data from Codeforces API:', error.message);
+        return false;
     }
-  };
-  
-  module.exports = {
+}
+
+const verify = async (id, index, userId) => {
+    return new Promise(async (resolve, reject) => {
+        let verified = false;
+        let intervalID;
+        intervalID = setInterval(async () => {
+            verified = await myCallback(id, index, userId);
+            if (verified) {
+                clearInterval(intervalID);
+                clearTimeout(timeoutID);
+                resolve(true);
+            }
+        }, 3000);
+
+        const timeoutID = setTimeout(() => {
+            clearInterval(intervalID);
+            resolve(false);
+        }, 60000);
+    });
+};
+
+module.exports = {
     verify
-  };
-  
+};
